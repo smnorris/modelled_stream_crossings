052020fa0507ed0a0630f463d9b502defb8166b4
Simon Norris <snorris@hillcrestgeo.ca>
2020-09-12 17:52:00 -0700

    create indexes on correct table and add the rest of the duplicate removal code

--- 02_process.sh
+++ 02_process.sh
@@ -6,32 +6,35 @@ psql -f sql/01_create_output_table.sql
 
 # load preliminary crossings by source and watershed group
 time psql -t -P border=0,footer=no \
--c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code IN ('VICT','COWN','SANJ')" \
+-c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code" \
     | sed -e '$d' \
     | parallel --colsep ' ' psql -f sql/02_intersect_dra.sql -v wsg={1}
 
 time psql -t -P border=0,footer=no \
--c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code IN ('VICT','COWN','SANJ')" \
+-c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code" \
     | sed -e '$d' \
     | parallel --colsep ' ' psql -f sql/03_intersect_ften.sql -v wsg={1}
 
 time psql -t -P border=0,footer=no \
--c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code IN ('VICT','COWN','SANJ')" \
+-c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code" \
     | sed -e '$d' \
     | parallel --colsep ' ' psql -f sql/04_intersect_ogc.sql -v wsg={1}
 
 time psql -t -P border=0,footer=no \
--c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code IN ('VICT','COWN','SANJ')" \
+-c "SELECT ''''||watershed_group_code||'''' FROM whse_basemapping.fwa_watershed_groups_poly WHERE watershed_group_code" \
     | sed -e '$d' \
     | parallel --colsep ' ' psql -f sql/05_intersect_railway.sql -v wsg={1}
 
 
 # create indexes
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (transport_line_id);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (ften_road_segment_id);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (og_road_segment_permit_id);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (og_petrlm_dev_rd_pre06_pub_id);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (railway_track_id);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (blue_line_key);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings (linear_feature_id);"
-psql -c "CREATE INDEX ON fish_passage.preliminary_stream_crossings USING GIST (geom);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (transport_line_id);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (ften_road_segment_id);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (og_road_segment_permit_id);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (og_petrlm_dev_rd_pre06_pub_id);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (railway_track_id);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (blue_line_key);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings (linear_feature_id);"
+psql -c "CREATE INDEX ON fish_passage.modelled_stream_crossings USING GIST (geom);"
+
+# remove duplicate crossings introduced by using multiple sources
+psql -f sql/remove_duplicates.sql
--- sql/06_remove_duplicates.sql
+++ sql/06_remove_duplicates.sql
@@ -1,47 +1,131 @@
--- data sources in order of priority (spatially)
--- (we don't consider railways, happily there is just one source)
+-- Remove crossing duplication from multiple data sources.
+
+-- When removing duplicates, we consider sources in this order
+-- of presumed spatial accuracy (FTEN and OGC are tenures, not as-built)
 -- 1. DRA
 -- 2. FTEN
 -- 3. OGC permits
 -- 4. OGC pre-06
+-- (we don't consider railways, happily there is just one source)
 
+-- As FTEN data can be the most useful for resource road fish passage work
+-- we want to retain FTEN attributes as best as possible - transfer the FTEN ids
+-- to the DRA based points via a nearest neighbour spatial join.
 
--- But, FTEN data can be the most useful for resource road fish passage work,
--- so we want to retain these as best as possible.
-
--- For each DRA crossing, find the closest FTEN crossing (within 20m)
+-- For FTEN crossings within 20m of a DRA point, assign the closest DRA point the FTEN id
 WITH matched_ften_xings AS
 (
-    SELECT
-      t1.preliminary_crossing_id as id,
-      t1.transport_line_id,
-      nn.preliminary_crossing_id,
-      nn.ften_road_segment_id
-    FROM fish_passage.preliminary_stream_crossings t1
+   SELECT
+      t1.modelled_crossing_id as modelled_crossing_id_ften,
+      t1.ften_road_segment_id,
+      nn.modelled_crossing_id as modelled_crossing_id_keep,
+      nn.transport_line_id
+    FROM fish_passage.modelled_stream_crossings t1
     CROSS JOIN LATERAL
       (SELECT
-       preliminary_crossing_id,
-       ften_road_segment_id,
-       ST_Distance(t1.geom, t2.geom) as dist
-       FROM fish_passage.preliminary_stream_crossings t2
-       WHERE t2.ften_road_segment_id IS NOT NULL
+         modelled_crossing_id,
+         transport_line_id,
+         ST_Distance(t1.geom, t2.geom) as dist
+       FROM fish_passage.modelled_stream_crossings t2
+       WHERE t2.transport_line_id IS NOT NULL
        ORDER BY t1.geom <-> t2.geom
        LIMIT 1) as nn
-    WHERE t1.transport_line_id IS NOT NULL
+    WHERE t1.ften_road_segment_id IS NOT NULL
     AND nn.dist < 20
-    ORDER BY t1.preliminary_crossing_id
+    ORDER BY t1.modelled_crossing_id
 )
-
--- If there is a match, give the DRA crossing the ften_road_segment_id value from the match
--- (the select and update takes about 4min on my db)
-UPDATE fish_passage.preliminary_stream_crossings x
+UPDATE fish_passage.modelled_stream_crossings x
 SET ften_road_segment_id = y.ften_road_segment_id
 FROM matched_ften_xings y
-WHERE x.preliminary_crossing_id = y.id;
+WHERE x.modelled_crossing_id = y.modelled_crossing_id_keep;
+
+-- Now delete FTEN crossings with IDs that were transferred over to the DRA crossings
+DELETE FROM fish_passage.modelled_stream_crossings
+WHERE ften_road_segment_id IN
+(SELECT ften_road_segment_id
+ FROM fish_passage.modelled_stream_crossings
+ WHERE transport_line_id IS NOT NULL
+ AND ften_road_segment_id IS NOT NULL)
+AND transport_line_id IS NULL;
 
 
--- with the FTEN id now stored in the DRA based crossing, we can delete the FTEN crossings
-DELETE
+-- Find OGC permit crossings within 20m of DRA/FTEN crossings
+WITH matched_ogc_xings AS
+(
+   SELECT
+      t1.modelled_crossing_id as modelled_crossing_id_ogc,
+      t1.og_road_segment_permit_id,
+      nn.modelled_crossing_id as modelled_crossing_id_keep,
+      nn.transport_line_id,
+      nn.ften_road_segment_id
+    FROM fish_passage.modelled_stream_crossings t1
+    CROSS JOIN LATERAL
+      (SELECT
+         modelled_crossing_id,
+         transport_line_id,
+         ften_road_segment_id,
+         ST_Distance(t1.geom, t2.geom) as dist
+       FROM fish_passage.modelled_stream_crossings t2
+       WHERE t2.transport_line_id IS NOT NULL
+       OR t2.ften_road_segment_id IS NOT NULL
+       ORDER BY t1.geom <-> t2.geom
+       LIMIT 1) as nn
+    WHERE t1.og_road_segment_permit_id IS NOT NULL
+    AND nn.dist < 20
+    ORDER BY t1.modelled_crossing_id
+)
+UPDATE fish_passage.modelled_stream_crossings x
+SET og_road_segment_permit_id = y.og_road_segment_permit_id
+FROM matched_ogc_xings y
+WHERE x.modelled_crossing_id = y.modelled_crossing_id_keep;
 
--- next, go ahead and delete OGC road crossings within 20m of DRA crossings
+DELETE FROM fish_passage.modelled_stream_crossings
+WHERE og_road_segment_permit_id IN
+(SELECT og_road_segment_permit_id
+ FROM fish_passage.modelled_stream_crossings
+ WHERE (transport_line_id IS NOT NULL OR ften_road_segment_id IS NULL)
+ AND og_road_segment_permit_id IS NOT NULL)
+AND transport_line_id IS NULL AND ften_road_segment_id IS NULL;
 
+
+-- Find OGC pre06 crossings within 20m of DRA/FTEN crossings
+-- (since we already combined the ogc crossings in the same-source
+-- duplicate removal, don't bother comparing these to ogc permits
+-- (this means we miss comparing ogc and ogc pre06 between 10 & 20m but
+-- that should not be a significant issue)
+WITH matched_ogcpre06_xings AS
+(
+   SELECT
+      t1.modelled_crossing_id as modelled_crossing_id_ogc,
+      t1.og_petrlm_dev_rd_pre06_pub_id,
+      nn.modelled_crossing_id as modelled_crossing_id_keep,
+      nn.transport_line_id,
+      nn.ften_road_segment_id
+    FROM fish_passage.modelled_stream_crossings t1
+    CROSS JOIN LATERAL
+      (SELECT
+         modelled_crossing_id,
+         transport_line_id,
+         ften_road_segment_id,
+         ST_Distance(t1.geom, t2.geom) as dist
+       FROM fish_passage.modelled_stream_crossings t2
+       WHERE t2.transport_line_id IS NOT NULL
+       OR t2.ften_road_segment_id IS NOT NULL
+       ORDER BY t1.geom <-> t2.geom
+       LIMIT 1) as nn
+    WHERE t1.og_petrlm_dev_rd_pre06_pub_id IS NOT NULL
+    AND nn.dist < 20
+    ORDER BY t1.modelled_crossing_id
+)
+UPDATE fish_passage.modelled_stream_crossings x
+SET og_petrlm_dev_rd_pre06_pub_id = y.og_petrlm_dev_rd_pre06_pub_id
+FROM matched_ogcpre06_xings y
+WHERE x.modelled_crossing_id = y.modelled_crossing_id_keep;
+
+DELETE FROM fish_passage.modelled_stream_crossings
+WHERE og_petrlm_dev_rd_pre06_pub_id IN
+(SELECT og_petrlm_dev_rd_pre06_pub_id
+ FROM fish_passage.modelled_stream_crossings
+ WHERE (transport_line_id IS NOT NULL OR ften_road_segment_id IS NULL)
+ AND og_petrlm_dev_rd_pre06_pub_id IS NOT NULL)
+AND transport_line_id IS NULL AND ften_road_segment_id IS NULL;
